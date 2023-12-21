0ff856ace11e51072b13059230d6e5ff2a134591
kumarshivu12 <2020uce0060@iitjammu.ac.in>
2023-12-21 17:02:54 +0530

    configuring multer and cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,37 @@
+import fs from "fs";
+import { v2 as cloudinary } from "cloudinary";
+import { ApiError } from "../utils/ApiError.js";
+
+//configuring cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) {
+      return null;
+    }
+
+    //upload files on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    //deleting file from server(local...)
+    fs.unlinkSync(localFilePath);
+
+    //sending response
+    return response;
+  } catch (error) {
+    //deleting file from server(local...)
+    fs.unlinkSync(localFilePath);
+    throw new ApiError(
+      400,
+      error?.message ||
+        "something went wrong while uploading file on cloudinary"
+    );
+  }
+};
